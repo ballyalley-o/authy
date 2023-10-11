8b0a85bfaf05ad1fe42f39ea9fee43225d813466
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-11 16:23:33 +1300

    form header update bool

--- client/src/components/Form/FormHeader.jsx
+++ client/src/components/Form/FormHeader.jsx
@@ -3,16 +3,17 @@ import * as _ from '@styles'
 // @constants
 import { HEADER } from '@constants'
 
-const FormHeader = ({ register }) => {
+const FormHeader = ({ register, update }) => {
   return (
     <h1 className={_.StyledSignInH1}>
-      {register ? HEADER.register : HEADER.signin}
+      {register ? HEADER.register : update ? HEADER.update : HEADER.signin}
     </h1>
   )
 }
 
 FormHeader.propTypes = {
   register: PropTypes.bool,
+  update: PropTypes.bool,
 }
 
 export default FormHeader
