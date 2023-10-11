a162ee1b642c8154cea0ee1e2fbe880f22d079b9
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-11 15:09:30 +1300

    sets path constants

---
+++ client/src/assets/sets/sets.js
@@ -0,0 +1,16 @@
+import * as _ from '@styles'
+
+export const nav_dropdown = {
+  id: 'username',
+  className: _.StyledNavDropdown,
+}
+
+export const navbar = {
+  bg: 'warning',
+  expand: 'lg',
+  collapseOnSelect: true,
+}
+
+export const nav_toggle = {
+  id: 'basic-navbar-nav',
+}
