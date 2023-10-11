cd01aca373956ab99ca938694c5839874f5c3686
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-11 18:07:21 +1300

    comments on styled classnames

--- client/src/assets/styles/styles.js
+++ client/src/assets/styles/styles.js
@@ -7,12 +7,15 @@ export const StyledTitle = classNames('')
 export const StyledLoader = classNames('loader')
 export const StyledContainerMargin = classNames('mt-10')
 export const StyledContainer = classNames('flex-row justify-center mt-10')
+// @nav
 export const StyledNavContainer = classNames('container')
 export const StyledNavBrand = classNames('nav_brand')
 export const StyledNavDropdown = classNames('text-primary')
 export const StyledNavCollapse = classNames('ms-auto text-black')
+// @sign in - page
 export const StyledInputMargin = classNames('my-3')
 export const StyledSignInH1 = classNames('text-6xl my-6')
+// @form
 export const StyledFormRow = classNames('py-3')
 export const StyledFormLink = classNames('link-default')
 export const StyledFormBtn = classNames('my-10 px-4 button-default')
