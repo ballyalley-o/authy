83e4f6c9fcb46f68f9a2a140fa2dac0d6375003b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-11 16:30:30 +1300

    path redirect fix

--- client/src/components/Route/PrivateRoute.jsx
+++ client/src/components/Route/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+// @constants
+import { PATH } from '@constants'
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth)
-  return userInfo ? <Outlet /> : <Navigate to='/signin' replace />
+  return userInfo ? <Outlet /> : <Navigate to={PATH.signin} replace />
 }
 
 export default PrivateRoute
