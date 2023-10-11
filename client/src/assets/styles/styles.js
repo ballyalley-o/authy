a49ee1d2f14ec67fec1d3465cf59e494ea8ac073
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-11 15:10:16 +1300

    styles refactor

--- client/src/assets/styles/styles.js
+++ client/src/assets/styles/styles.js
@@ -5,5 +5,9 @@ import classNames from 'classnames'
  */
 export const StyledTitle = classNames('')
 export const StyledLoader = classNames('loader')
-export const StyledContainer = classNames('')
+export const StyledContainerMargin = classNames('mt-10')
+export const StyledContainer = classNames('flex-row justify-center mt-10')
+export const StyledNavContainer = classNames('container')
 export const StyledNavBrand = classNames('nav_brand')
+export const StyledNavDropdown = classNames('text-primary')
+export const StyledNavCollapse = classNames('ms-auto text-black')
